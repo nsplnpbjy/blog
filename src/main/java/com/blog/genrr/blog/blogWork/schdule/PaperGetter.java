490e15272d529206c9e92a2d9917fd029d9fa15b
nsplnpbjy <nsplnp@126.com>
2021-04-10 16:26:28 +0800

    增加了异步任务同时爬取两个作者的文章

--- src/main/java/com/blog/genrr/blog/blogWork/schdule/PaperGetter.java
+++ src/main/java/com/blog/genrr/blog/blogWork/schdule/PaperGetter.java
@@ -1,9 +1,12 @@
 package com.blog.genrr.blog.blogWork.schdule;
 
+import com.blog.genrr.blog.blogWork.config.PaperUrlProperties;
 import com.blog.genrr.blog.blogWork.mapper.PapersMapper;
 import com.blog.genrr.blog.blogWork.utils.PaperGetterUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -13,15 +16,26 @@ import javax.annotation.Resource;
  * @author nsplnpbjy
  */
 @Slf4j
+@EnableConfigurationProperties(PaperUrlProperties.class)
 @Component
+@EnableAsync
 public class PaperGetter {
 
     @Resource
     PapersMapper papersMapper;
 
-    @Scheduled(initialDelay = 1000,fixedRate = 2678400000L)
-    public void getPaper(){
-        PaperGetterUtil.marxEngels(papersMapper);
+    @Resource
+    PaperUrlProperties paperUrlProperties;
 
+    @Async
+    @Scheduled(initialDelay = 1000,fixedRate = 2678400000L)
+    public void getPaperOne(){
+        PaperGetterUtil.paperGetter(papersMapper,paperUrlProperties.getMarxUrl());
+    }
+
+    @Async
+    @Scheduled(initialDelay = 1000,fixedRate = 2678400000L)
+    public void getPaperTwo(){
+        PaperGetterUtil.paperGetter(papersMapper,paperUrlProperties.getLeninUrl());
     }
 }
